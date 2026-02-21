88e5e11e54654327f954fa126851cb6d62c29ef7
АлексЗУ <kilzut99@gmail.com>
2026-02-21 20:02:42 +0300

    New

---
+++ script.js
@@ -0,0 +1,19 @@
+// ЗАДАНИЕ 1: Валидация
+function checkLogin() {
+  const age = document.getElementById("ageInput").value;
+  const pass = document.getElementById("passInput").value;
+  const res = document.getElementById("loginResult");
+
+  // Вы пишете логику здесь...
+  // Подсказка: используйте Early Return
+}
+
+// ЗАДАНИЕ 2: Скидки
+function calculateDiscount() {
+  const sum = Number(document.getElementById("sumInput").value);
+  const res = document.getElementById("discountResult");
+
+  // Вы пишете логику здесь...
+}
+
+// Остальные функции аналогично...
